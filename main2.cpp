971cfe3344be8e9b7c1ed57570a78bb39f1e7feb
Ha-154 <nguyentranp30@gmail.com>
2025-06-02 17:27:26 +0800

    add main2.cpp

---
+++ main2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void foo() {
+
+}
+
+void bar() {
+    cout << "It’s bar!!" << endl;
+}
+ 
+int main() {
+    cout << "Hello, World!" << endl;
+
+    bar();
+    return 0;
+}
