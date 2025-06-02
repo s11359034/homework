0a68d1aa277b0fc7268fa75e7087fffc24750642
s11359034 <s11359034@gm2.nutn.edu.tw>
2025-06-02 17:18:14 +0800

    sec_f

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void foo() {
-
+    cout << "It’s foo!!" << endl;
 }
 
 void bar() {
@@ -11,5 +11,7 @@ void bar() {
  
 int main() {
     cout << "Hello, World!" << endl;
+
+    foo();
     return 0;
 }
